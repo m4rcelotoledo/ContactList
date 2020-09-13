ebdd3d572413d050761890c6b601dad90fce9c2f
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-09-13 18:32:23 -0300

    Update test to ensure uniqueness validation

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 describe Contact, type: :model do
+  subject(:new_contact) { create(:contact) }
+
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:name) }
