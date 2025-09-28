d04dbffe474cb38c4c5343caea7682691d7c296c
Marcelo Toledo <marcelotoledo5000@gmail.com>
2025-09-28 19:05:12 -0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,14 +13,18 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
 module ContactList
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.load_defaults 7.0
+
+    # Disable Host Authorization completely
+    config.hosts.clear
+    config.middleware.delete ActionDispatch::HostAuthorization
 
     # Don't generate system test files.
     # config.generators.system_tests = nil
